09e6ea83cea7e11cef54846747f83b558ffabe5a
Tom Schmidt <z0phim@yahoo.com>
2015-02-27 11:02:57 -0800

    Added root index route and view with bootstrap.

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var engine = require('express-dot-engine');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+var game  = require('./routes/game.js');
+
+//configure app
+app.engine('dot', engine.__express);
+app.set('view engine', 'dot');
+app.set('views', path.join(__dirname, './views'));
+
+//use middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
+
+//set routes
+app.use('/', index);
+app.use('/game/', game);
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
